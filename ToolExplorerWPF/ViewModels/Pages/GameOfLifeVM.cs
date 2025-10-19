bfc233c8275953802155900b56f370ec285b8383
IchiSamaFR <djordan-dsousa@hotmail.fr>
2025-10-19 17:18:26 +0200

    feature - GameOfLife generation selection

--- ToolExplorerWPF/ViewModels/Pages/GameOfLifeVM.cs
+++ ToolExplorerWPF/ViewModels/Pages/GameOfLifeVM.cs
@@ -23,15 +23,18 @@ namespace ToolExplorerWPF.ViewModels.Pages
     public partial class GameOfLifeVM : ObservableObject, INavigationAware
     {
         private bool _isInitialized = false;
+        private int _inputDelay = 200;
+        private CancellationTokenSource? _cancellationWidthToken;
+        private CancellationTokenSource? _cancellationHeightToken;
 
         [ObservableProperty]
-        private GameEngine _gameEngine;
+        private GameEngine _gameEngine = null!;
 
         // View
         [ObservableProperty]
-        private int _rows = 20;
+        private int _width = 30;
         [ObservableProperty]
-        private int _columns = 20;
+        private int _height = 30;
         [ObservableProperty]
         private int _speedDelay = 100;
 
@@ -49,10 +52,76 @@ namespace ToolExplorerWPF.ViewModels.Pages
         [ObservableProperty]
         private bool _isRunning = false;
 
+        public int Generation
+        {
+            get
+            {
+                return GameEngine?.Generation ?? 0;
+            }
+            set
+            {
+                if (GameEngine == null)
+                {
+                    return;
+                }
+                GameEngine.GoToGeneration(Math.Min(Math.Max(value, 0), TotalGenerations));
+                OnPropertyChanged(nameof(GameEngine));
+            }
+        }
+        public int TotalGenerations
+        {
+            get
+            {
+                return GameEngine?.TotalGenerations ?? 0;
+            }
+        }
+
         // Collection observable pour le binding
         [ObservableProperty]
         private ICell[,] _cells = new ICell[0, 0];
 
+        partial void OnWidthChanged(int value)
+        {
+            if (_cancellationWidthToken != null)
+            {
+                _cancellationWidthToken.Cancel();
+            }
+            _cancellationWidthToken = new CancellationTokenSource();
+            _ = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    await Task.Delay(_inputDelay, _cancellationWidthToken.Token);
+                    GameEngine.Grid.SetWidth(value);
+                    Cells = GameEngine.Grid.Cells;
+                }
+                catch (TaskCanceledException)
+                {
+                    // Task was cancelled, do nothing
+                }
+            });
+        }
+        partial void OnHeightChanged(int value)
+        {
+            if(_cancellationHeightToken != null)
+            {
+                _cancellationHeightToken.Cancel();
+            }
+            _cancellationHeightToken = new CancellationTokenSource();
+            _ = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    await Task.Delay(_inputDelay, _cancellationHeightToken.Token);
+                    GameEngine.Grid.SetHeight(value);
+                    Cells = GameEngine.Grid.Cells;
+                }
+                catch (TaskCanceledException)
+                {
+                    // La tâche a été annulée, ne rien faire
+                }
+            });
+        }
         partial void OnMinSurvivalNeighborsChanged(int value)
         {
             if (value < 0)
@@ -66,6 +135,13 @@ namespace ToolExplorerWPF.ViewModels.Pages
                 MaxSurvivalNeighbors = 4;
             else if (value < MinSurvivalNeighbors)
                 MaxSurvivalNeighbors = MinSurvivalNeighbors;
+        }
+        partial void OnBirthNeighborsChanged(int value)
+        {
+            if (value < 0)
+                BirthNeighbors = 0;
+            else if (value > 4)
+                BirthNeighbors = 4;
         }
 
         public void OnNavigatedTo()
@@ -89,7 +165,7 @@ namespace ToolExplorerWPF.ViewModels.Pages
         {
             IsStarted = false;
 
-            var grid = new ObservableGrid(Columns, Rows);
+            var grid = new ObservableGrid(Width, Height);
             var rules = new ModulableRules(MinSurvivalNeighbors, MaxSurvivalNeighbors, BirthNeighbors);
             GameEngine = GameEngineFactory.CreateCustomGameEngine(grid, rules);
 
@@ -121,10 +197,11 @@ namespace ToolExplorerWPF.ViewModels.Pages
         }
 
         [RelayCommand]
-        public void NextAge()
+        public void Reset()
         {
-            GameEngine.NextGeneration();
-            OnPropertyChanged(nameof(GameEngine));
+            IsRunning = false;
+            IsStarted = false;
+            GameEngine.Reset();
         }
 
         [RelayCommand]
@@ -138,20 +215,35 @@ namespace ToolExplorerWPF.ViewModels.Pages
         }
 
         [RelayCommand]
-        public void Reset()
+        public void Clear()
         {
             IsRunning = false;
-            Generate();
+            GameEngine.Reset();
+            OnPropertyChanged(nameof(Generation));
+            OnPropertyChanged(nameof(TotalGenerations));
+            OnPropertyChanged(nameof(Cells));
         }
 
         private async Task RunGenerationsAsync()
         {
-            IsStarted = true;
             while (IsRunning)
             {
-                NextAge();
+                NextGeneration();
                 await Task.Delay(SpeedDelay);
             }
+        }
+
+        [RelayCommand]
+        public void NextGeneration()
+        {
+            if (!IsStarted)
+            {
+                GameEngine.StartGeneration();
+                IsStarted = true;
+            }
+            GameEngine.NextGeneration();
+            OnPropertyChanged(nameof(Generation));
+            OnPropertyChanged(nameof(TotalGenerations));
         }
     }
 }
